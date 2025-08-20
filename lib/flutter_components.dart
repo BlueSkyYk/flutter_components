54352411730f29a7be396d19c67de9bd320ae160
yinkai <kai.yin@fanyou-online.com>
2025-08-20 10:55:01 +0800

    调整忽略文件，移除lib/flutter_components.dart

--- lib/flutter_components.dart
+++
@@ -1,7 +0,0 @@
-library flutter_components;
-
-export 'component/app_route_observer.dart';
-export 'component/keep_alive_wrapper.dart';
-export 'base/base_page.dart';
-export 'base/base_controller.dart';
-export 'base/base_item_controller.dart';
